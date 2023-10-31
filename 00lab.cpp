faf5cad213b50799b4fec90824a54b5201716c53
Tasnimul Hasan <67048618+tasnimul11@users.noreply.github.com>
2023-10-31 21:02:07 +0600

    First c++ program at AUST LAB
    Basic output

---
+++ 00lab.cpp
@@ -0,0 +1,12 @@
+//basic Lab introduction on 30Oct'23
+
+#include <iostream>
+using namespace std;
+int main()
+
+{
+    cout<<"Bismillah"<<endl; //endl is subject to start a new line(paragraph)
+    cout<<"Hello AUST"<<endl;
+
+    return 0;
+}
