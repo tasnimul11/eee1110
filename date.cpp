5ef2e169a002973fa185fcd8d66509bbf049e30a
Tasnimul Hasan <67048618+tasnimul11@users.noreply.github.com>
2023-11-08 23:21:51 +0600

    Basic Date output

---
+++ date.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout<<"This code is about showing static date"<<endl;
+
+    cout<<"Today is "<< "November " << 8 << " " << 2023 <<endl;
+
+    return 0;
+}
