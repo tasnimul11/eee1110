510153f2a21f6b1e606145e23c643428c182c355
Tasnimul Hasan <67048618+tasnimul11@users.noreply.github.com>
2023-11-11 10:24:23 +0600

    Introduction to OUTPUT

---
+++ Ex-2_intro.cpp
@@ -0,0 +1,18 @@
+/*
+prints
+Hello world
+Hello AUST
+This is a c++ program
+*/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout<< "Hel" <<"lo wo" << "rld"<<endl;
+    cout<< "Hello"<< " AUST" <<endl;
+    cout<< "This i"<< "s a c++" << " pro"<<"gram";
+
+    return 0;
+
+}
