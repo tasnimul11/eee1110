bf0d22e3feebfc0f6617aefa76124a8672fd34ed
Tasnimul Hasan <67048618+tasnimul11@users.noreply.github.com>
2023-11-13 02:12:07 +0600

    Fav. Colors

---
+++ Laboratory01/PostLab Problems-01/Lab-1_Prob-03-abc.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+
+{
+    cout<<"Names of my SIX Fav. Colors using comma & space: "<<endl;
+    cout<<"Orange, Red, Blue, Magenta, Violet, Green"<<endl<<endl;     //a
+
+    cout<<"Names of my SIX Fav. Colors in 6 rows "<<endl;
+    cout<<"Orange\nRed\nBlue\nMagenta\nViolet\nGreen"<<endl<<endl;      //b
+
+    cout<<"Names of my SIX Fav. Colors in 3 rows, 2 columns "<<endl;
+    cout<<"Orange\tRed\nBlue\tMagenta\nViolet\tGreen"<<endl<<endl;      //c
+
+    cout<<"Names of my SIX Fav. Colors in 2 rows, 3 columns "<<endl;
+    cout<<"Orange\tRed\tBlue\nMagenta\tViolet\tGreen"<<endl;            //extra
+
+
+}
