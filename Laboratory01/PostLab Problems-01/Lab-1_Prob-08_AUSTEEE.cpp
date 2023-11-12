d1b43a98bbf1ccbb7dd7237c3496e68bfde747c1
Tasnimul Hasan <67048618+tasnimul11@users.noreply.github.com>
2023-11-13 02:16:56 +0600

    Tricky Backslash, so good

---
+++ Laboratory01/PostLab Problems-01/Lab-1_Prob-08_AUSTEEE.cpp
@@ -0,0 +1,14 @@
+//This seems simple but TRICKYYY, there's a fun!!!
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"    ___   __  __ ____ ______  ______ _____ _____"<<endl;
+    cout<<"   /   | / / / / ___/ _  __/ / ____/ ____/ ____/"<<endl;
+    cout<<"  / /| |/ / / /\\\__ \\\  / /   / __/ / __/ / __/"<<endl;
+    cout<<" / ___ / /_/ / __/ / / /   / /___/ /___/ /___"<<endl;
+    cout<<"/_/  |_\\\____//___ / /_/   /_____/ ____/_____/"<<endl;
+
+    /*THE TRICK IS If you want to print a backslash (\\), you need to use 3 backslashes (\\\) in your string.
+     The first backslash escapes the second one, so a single backslash is printed*/
+}
