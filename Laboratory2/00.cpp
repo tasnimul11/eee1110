9b068fe64cf6f9e115d464309369a57b2289e8b4
Tasnimul Hasan <67048618+tasnimul11@users.noreply.github.com>
2023-11-21 22:06:22 +0600

    char

---
+++ Laboratory2/00.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    char c1;
+    c1='a';  //must be in single quotation mark to print character
+
+    cout<<c1<<endl;
+
+
+    return 0;
+
+}
