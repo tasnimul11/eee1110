219e96f28d80e83cd05c5e238db3034e946322fa
Tasnimul Hasan Prottoy <67048618+tasnimul11@users.noreply.github.com>
2024-01-28 23:34:56 +0600

    Solution to Lab-07_Prob-02

--- Laboratory07/PostLab_Problems-07/Lab-07_Prob-02.cpp
+++ Laboratory07/PostLab_Problems-07/Lab-07_Prob-02.cpp
@@ -1,1 +1,36 @@
+#include <iostream>
+using namespace std;
 
+
+void swap(int *p, int *q, int *r, int *s)
+{
+    int temp1, temp2;
+    temp1 =*p;
+    temp2 = *q;
+    *p = *s;
+    *q = *r;
+    *r = temp2;
+    *s = temp1;
+
+}
+
+int main()
+{
+    int p, q, r, s;
+    cout<<"Enter 4 values: ";
+    cin>> p >> q >>r >> s;
+
+    cout<<endl<<"Before swapping : "<<endl;
+    cout<<"P = "<< p <<endl;
+    cout<<"Q = "<< q <<endl;
+    cout<<"R = "<< r <<endl;
+    cout<<"S = "<< s <<endl;
+
+    swap(&p, &q, &r, &s);
+
+    cout<<endl<<"After Swapping : " <<endl;
+    cout<<"P = "<< p <<endl;
+    cout<<"Q = "<< q <<endl;
+    cout<<"R = "<< r <<endl;
+    cout<<"S = "<< s <<endl;
+}
